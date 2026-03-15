d369a4bceb1ebfd15c0303992f3b075f406d4431
beyz100 <beyzayuz@gmail.com>
2026-03-15 15:17:56 +0300

    Corrected urlpatterns

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'restaurants'
 
 urlpatterns = [
-    path('', views.home_view, name='home'),
-    path('restaurants/', views.restaurant_list_view, name='list'),
-    path('restaurants/<int:id>/', views.restaurant_detail_view, name='detail'),
-    path('about/', views.about_view, name='about'),
-    path('contact/', views.contact_view, name='contact'),
+    path('', views.home, name='home'),
+    path('restaurants/', views.restaurant_list, name='list'),
+    path('restaurants/<int:id>/', views.restaurant_detail, name='detail'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
